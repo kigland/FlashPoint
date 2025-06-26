c4bebf756ada0de478dea843be32e0571e8457de
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2025-06-26 01:03:20 +0100

    feat: add title

--- sdk/testman/testSetTxt/main.go
+++ sdk/testman/testSetTxt/main.go
@@ -9,8 +9,9 @@ import (
 
 func main() {
 	client := sdk.NewClient("http://127.0.0.1:8080", "1234567890")
-	_, err := client.Set("test", "testValue", 10*time.Second, "txt", "text/plain")
+	resp, err := client.Set("", "testValue", 10*time.Second, "txt", "text/plain")
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println(resp.Key)
 }
